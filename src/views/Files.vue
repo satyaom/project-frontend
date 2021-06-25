7a34618e823010b6e82a40cdc49969a1fb309fd7
satyam <satyaom1998@gmail.com>
2021-06-25 20:28:39 +0530

    added fileinfo and css to showcase

--- src/views/Files.vue
+++ src/views/Files.vue
@@ -1,7 +1,14 @@
 <template>
-    
-    <div v-for="item in qr" v-bind:key="item"> 
-        <img :src="item" alt='not loaded' width="150" height="150"/>
+    <div class="messages">
+    <div v-for="item in doc" v-bind:key="item" class="card"> 
+        <img :src="item.qrcode" alt='not loaded' width="150" height="150"/>
+        <div class="container">
+        <h4>FILE ID: {{item.fileid}}</h4>
+        <h4>NAME: {{item.name}}</h4>
+        <h4>TAMPERED: {{item.tampered}}</h4>
+        <h4>FILE TYPE: {{item.filetype}}</h4> 
+        </div>
+    </div>
     </div>
 </template>
 
@@ -11,27 +18,45 @@ export default {
     name: "file",    
     data() {    
         return {
-            qr : [],
+            doc : [],
         }    
     },    
     async created() {
         let files = await getDataDB();
         for(let i = 0; i < files.length; i++){
-            await this.qr.push(files[i].qrcode);
+            await this.doc.push(files[i]);
         }
     }
 }
 </script>
 
 <style scoped>
-.messages { 
+.card {
+  margin-top: 16px;
+  margin-right: 16px;
+  margin-left: 16px;
+  margin-bottom: 16px;
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+  width: 25%;
+}
+
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+}
+
+.container {
+    overflow: auto;
+  padding: 2px 16px;
+}
+.messages {
     display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100%;
-    width: 100%;
-    font-size: 40px;
-    color: rgb(167, 167, 167);
+  flex-wrap: wrap;
+  padding: 0 4px;
+    text-align: center;
     font-weight: 600;
+    height: 900px;
+    overflow:auto;
+    
 }
 </style>
